cc7b698a9a734b5ca0468d67e02aab6be5e6df12
kbrv37 <Al.pizano@motorolasolutions.com>
2020-12-14 21:21:01 -0600

    autowire petservice dep

--- src/main/java/guru/springframework/sfgdi/controllers/PetController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/PetController.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.PetService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Created by jt on 12/28/19.
@@ -8,6 +9,11 @@ import guru.springframework.sfgdi.services.PetService;
 public class PetController {
 
     private final PetService petService;
+
+    @Autowired
+    public PetController(PetService petService) {
+        this.petService = petService;
+    }
 
     public String whichPetIsTheBest(){
         return petService.getPetType();
